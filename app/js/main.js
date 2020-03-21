683f14c19591e403653c732733cf0acc1f13a952
Andrea Carmisciano <andrea.carmisciano@gmail.com>
2020-03-21 16:35:57 +0100

    template

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,15 @@
+$( "#theform" ).submit(function( event ) {
 
+  event.preventDefault();
+
+  var $form = $( this ), url = $form.attr( "action" );
+
+  var posting = $.post( url, $form.serialize());
+
+  posting.done(function( data ) {
+    var content = data;
+    console.log(data);
+    $( "#result" ).append('<div class="taken-number">'+data.number+'</div>');
+    $( "#submit-btn").html('Prenotane un altro!')
+  });
+});
